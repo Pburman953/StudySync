ba216d962b490eb92e4df43cc45c5506fc531aee
kdhruv! <dhruv.kumar@students.plymouth.ac.uk>
2024-05-02 01:55:12 +0100

    Dhruv Kumar - UI fixes

--- Home2/app/src/main/java/com/example/home/DashboardActivity.java
+++ Home2/app/src/main/java/com/example/home/DashboardActivity.java
@@ -2,69 +2,61 @@ package com.example.home;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-
-//import com.example.home.databinding.FragmentDashboardBinding;
+import com.example.home.databinding.ActivityDashboardBinding;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-
-    ImageView Tracker;
+    ActivityDashboardBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        findViewById(R.id.button_reminder).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(DashboardActivity.this, ReminderActivity.class));
+
+
+
+
+        binding.bottomnavigation.setOnNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.home) {
+                // Handle click on home item
+                // You can start the new activity like this:
+                Intent homeIntent = new Intent(this, DashboardActivity.class);
+                startActivity(homeIntent);
+                return true;
+            } else if (item.getItemId() == R.id.notes) {
+                // Handle click on notes item
+                // Start another activity similarly
+                Intent notesIntent = new Intent(this, NotesActivity.class);
+                startActivity(notesIntent);
+                return true;
+            } else if (item.getItemId() == R.id.tracker) {
+                // Handle click on tracker item
+                // Start another activity similarly
+                Intent trackerIntent = new Intent(this, Tracker.class);
+                startActivity(trackerIntent);
+                return true;
+            } else if (item.getItemId() == R.id.settings) {
+                // Handle click on settings item
+                // Start another activity similarly
+                Intent settingsIntent = new Intent(this, Login.class);
+                startActivity(settingsIntent);
+                return true;
             }
+
+            // Add conditions for other items if needed
+            return false;
         });
 
-        findViewById(R.id.button_notes).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(DashboardActivity.this, NotesActivity.class));
-            }
-        });
 
-        Tracker = findViewById(R.id.ScreenTime);
-        Tracker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(DashboardActivity.this, Tracker.class);
-                startActivity(intent);
-            }
-        });
 
-        findViewById(R.id.button_calender).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity( new Intent(DashboardActivity.this, ManageReminderActivity.class));
-            }
-        });
+
+
+
+
     }
-
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
-
 }
