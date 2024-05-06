8413431bda0ac4407cb4dc10fe7d4033f464b536
Prithviraj Burman <prithviraj.burman107@gmail.com>
2024-05-06 16:17:21 +0100

    making sure reminder is double displayed on front page, plus button functionality to dashboard takes you to manage reminder page

--- Home2/app/src/main/java/com/example/home/DashboardActivity.java
+++ Home2/app/src/main/java/com/example/home/DashboardActivity.java
@@ -50,8 +50,9 @@ public class DashboardActivity extends AppCompatActivity {
 
         if (!remindersJson.isEmpty()) {
             Reminder[] reminderArray = gson.fromJson(remindersJson, Reminder[].class);
-            for (Reminder reminder : reminderArray) {
-                Values.RemindersList.add(reminder);
+            for (int i = 0; i < reminderArray.length; i++ ) {
+                if(!Values.RemindersList.contains(reminderArray[i]))
+                Values.RemindersList.add(reminderArray[i]);
             }
         }
 
